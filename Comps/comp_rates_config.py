68dc3357f76491319af3f44b82829a5e44131443
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2025-07-25 17:28:33 +0700

    chore: 3.4.2 data

--- Comps/comp_rates_config.py
+++ Comps/comp_rates_config.py
@@ -4,11 +4,11 @@ from os.path import dirname as path_dirname
 from os.path import join as path_join
 
 # don't add underscore, i.e. 2.2.1"_pf"
-RECENT_PHASE = "3.4.1"
+RECENT_PHASE = "3.4.2"
 
 # if no past phase, leave blank
 # add underscore, i.e. 2.2.1"_pf"
-past_phase = "3.3.4"
+past_phase = "3.4.1"
 
 parser = ArgumentParser()
 parser.add_argument("-a", "--all", action="store_true")
