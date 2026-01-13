79722515a01ab0fbaf24fb428fa907748a4cf499
Rhapsodya Piedro Asmorobangun <piedrorhapsodya@gmail.com>
2026-01-13 14:10:43 +0700

    refactor(combine raw chars): delete output_char file

--- Comps/combine_raw_chars.py
+++ Comps/combine_raw_chars.py
@@ -7,15 +7,17 @@ from comp_rates_config import RECENT_PHASE
 from send2trash import send2trash
 
 char_data: dict[str, dict[str, list[str]]] = {}
+char_data_path = "../data/raw_csvs_real/" + RECENT_PHASE + "_char.csv"
+enka_char_path = "../mihomo/output1_char.csv"
 print_data: list[list[str]] = []
 
 print("Opening files...")
 # with open("char_data.csv", 'r') as f:
-with open("../mihomo/output1_char.csv", encoding="UTF8") as f:
+with open(enka_char_path, encoding="UTF8") as f:
     reader = csv_reader(f, delimiter=",")
     print_data += [next(reader)]
     char_data_temp = list(reader)
-with open("../data/raw_csvs_real/" + RECENT_PHASE + "_char.csv") as f:
+with open(char_data_path) as f:
     reader = csv_reader(f, delimiter=",")
     headers = next(reader)
     char_data_temp += list(reader)
@@ -35,7 +37,8 @@ for uid, uid_char in char_data.items():
     for char in uid_char:
         print_data += [[uid, "", char] + uid_char[char]]
 
-send2trash("../data/raw_csvs_real/" + RECENT_PHASE + "_char.csv")
-with open("../data/raw_csvs_real/" + RECENT_PHASE + "_char.csv", "w", newline="") as f:
+send2trash(char_data_path)
+send2trash(enka_char_path)
+with open(char_data_path, "w", newline="") as f:
     csv_writer = csv_writer(f)
     csv_writer.writerows(print_data)
